b6dc02f5ef18c56c33fba4e4f990f7841106d9f8
Florian Bruggisser <florian@nexpose.ch>
2017-08-10 17:53:52 +0200

    implemented basic PIR tracking

--- pir.ino
+++ pir.ino
@@ -1,7 +1,21 @@
+int pirState = LOW;
+
 void setupPIR()
 {
+  pinMode(PIR_PIN, INPUT);
 }
 
 void loopPIR()
 {
+  // change detection
+  int input = digitalRead(PIR_PIN);
+  if (input == HIGH && pirState == LOW) {
+    pirState = HIGH;
+    Serial.println("PIR: Motion detected!");
+  }
+  else if (input == LOW && pirState == HIGH)
+  {
+    pirState = LOW;
+    Serial.println("PIR: No motion!");
+  }
 }
