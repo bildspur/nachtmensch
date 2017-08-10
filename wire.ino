5e748eac6f07231f07266714c4312ae370eca4c4
Florian Bruggisser <florian@nexpose.ch>
2017-08-10 17:55:14 +0200

    implemented wire as basic led control

--- wire.ino
+++ wire.ino
@@ -2,15 +2,18 @@ boolean isWireOn = false;
 
 void setupWire()
 {
+  wireOff();
 }
 
 void wireOff()
 {
   isWireOn = false;
+  analogWrite(EL_WIRE_PIN, 0);
 }
 
 void wireOn()
 {
   isWireOn = true;
+  analogWrite(EL_WIRE_PIN, 255);
 }
 
