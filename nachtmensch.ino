53e02e14bb416c95c0ee0de591b009b426a6a778
Florian Bruggisser <florian@bildspur.ch>
2018-11-12 22:41:42 +0100

    updated sketch

--- nachtmensch.ino
+++ nachtmensch.ino
@@ -10,21 +10,21 @@ extern "C" {
 
 // NodeMCU Digital Pins
 #define D0  16
-#define D1  5  // I2C Bus SCL (clock)
-#define D2  4  // I2C Bus SDA (data)
+#define D1  5  // I2C Bus SCL (clock) (save)
+#define D2  4  // I2C Bus SDA (data) (save)
 #define D3  0
 #define D4  2  // Same as "LED_BUILTIN", but inverted logic
-#define D5  14 // SPI Bus SCK (clock)
-#define D6  12 // SPI Bus MISO 
-#define D7  13 // SPI Bus MOSI
+#define D5  14 // SPI Bus SCK (clock) (save)
+#define D6  12 // SPI Bus MISO (save)
+#define D7  13 // SPI Bus MOSI (save)
 #define D8  15 // SPI Bus SS (CS)
 #define D9  3  // RX0 (Serial console)
 #define D10 1  // TX0 (Serial console)
 
 // Nachtmensch Pins
-#define LED_PIN D6
+#define LED_PIN D2
 #define EL_WIRE_PIN D7
-#define PIR_PIN D8
+#define PIR_PIN D1
 
 #define DEFAULT_LOOP_DELAY 10
 
